f80885704eab336045d1c2c1f89c8b3a1517f9bc
AngAcosta <acostavillaangel23@gmail.com>
2023-04-12 17:20:19 -0600

    MapsMakers

--- TheMovie/Controllers/VentaController.cs
+++ TheMovie/Controllers/VentaController.cs
@@ -7,11 +7,13 @@ namespace TheMovie.Controllers
         public ActionResult Venta()
         {
             ML.Cine cine = new ML.Cine();
+            ML.Result resultCine = BL.Cine.GetAll();
             ML.Result result = BL.Cine.Porcentaje();
 
             if (result.Correct)
             {
                 cine = (ML.Cine)result.Object; //unboxing
+                cine.Cines = resultCine.Objects;
                 return View(cine);
             }
             else
